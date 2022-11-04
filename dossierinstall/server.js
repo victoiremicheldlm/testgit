dbcbe3a2f0b84dc978989207e729e87150111698
VICTOIREMICHELDLM <victoiremicheldlm@gmail.com>
2022-11-04 15:26:24 +0100

    I have completed the exercise

---
+++ dossierinstall/server.js
@@ -0,0 +1,6 @@
+// server.js
+require('dotenv').config() // this line is mandatory when you want to read variables from the '.env' file 
+
+console.log(`I am "${process.env.MY_NAME}" wilder in "${process.env.MY_NAME}", and I love "${process.env.MY_NAME}".`);
+    
+console.log(`I am "${process.env.MY_NAME}"`);
